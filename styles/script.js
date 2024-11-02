b6450ad93e7931f4c8039718818ee553cd53a941
Denis Filatov <den.2797@bk.ru>
2024-11-02 21:12:23 +0300

    стаковое изменение

--- styles/script.js
+++ styles/script.js
@@ -18,3 +18,54 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Добавьте обработчик событий нажатия на экран для повышения вероятности воспроизведения
     window.addEventListener('touchstart', playVideo, { once: true });
 });
+
+
+// function modalClose (modal) {
+//     modal.classList.add('disable');
+//     modal.close();
+// }
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const modal = document.querySelector('dialog');
+//     const slider = modal.querySelector('.slider');
+//     modal.showModal();
+
+//     slider.addEventListener('change', evt => {
+//         if (evt.target.value !== '100') {
+//             evt.target.value = 0;
+//             return;
+//         }
+
+//         modalClose(modal);
+//     });
+
+// });
+
+// закрытия диалогового окна если клацать мимо
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  dialogElement.addEventListener("click", closeOnBackDropClick)
+
+function closeOnBackDropClick({ currentTarget, target }) {
+  const dialogElement = currentTarget
+  const isClickedOnBackDrop = target === dialogElement
+  if (isClickedOnBackDrop) {
+    dialogElement.close()
+  }
+}
