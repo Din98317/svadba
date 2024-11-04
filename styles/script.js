78d430fe6a61cb911a5e35e43bcb28045cc31bd4
Denis Filatov <den.2797@bk.ru>
2024-11-04 12:41:38 +0300

    css

--- styles/script.js
+++ styles/script.js
@@ -1,74 +1,50 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const video = document.getElementById('background-video');
-    // Проверка на воспроизведение видео
-    const playVideo = () => {
-        video.play().catch((error) => {
-            // Обработка ошибки воспроизведения
-            console.error('Ошибка воспроизведения видео:', error);
-            // Попробуйте воспроизвести видео снова после некоторой задержки
-            setTimeout(() => {
-                video.play();
-            }, 1000);
-        });
-    };
-    
-    // Попытка воспроизведения видео
-    playVideo();
+document.addEventListener("DOMContentLoaded", (event) => {
+  const video = document.getElementById("background-video");
+  // Проверка на воспроизведение видео
+  const playVideo = () => {
+    video.play().catch((error) => {
+      // Обработка ошибки воспроизведения
+      console.error("Ошибка воспроизведения видео:", error);
+      // Попробуйте воспроизвести видео снова после некоторой задержки
+      setTimeout(() => {
+        video.play();
+      }, 1000);
+    });
+  };
 
-    // Добавьте обработчик событий нажатия на экран для повышения вероятности воспроизведения
-    window.addEventListener('touchstart', playVideo, { once: true });
+  // Попытка воспроизведения видео
+  playVideo();
+
+  // Добавьте обработчик событий нажатия на экран для повышения вероятности воспроизведения
+  window.addEventListener("touchstart", playVideo, { once: true });
 });
 
-
-// function modalClose (modal) {
-//     modal.classList.add('disable');
-//     modal.close();
-// }
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const modal = document.querySelector('dialog');
-//     const slider = modal.querySelector('.slider');
-//     modal.showModal();
-
-//     slider.addEventListener('change', evt => {
-//         if (evt.target.value !== '100') {
-//             evt.target.value = 0;
-//             return;
-//         }
-
-//         modalClose(modal);
-//     });
-
-// });
-
-// закрытия диалогового окна если клацать мимо
-
+// анимация скрола
 function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-       change.target.classList.add('element-show');
-      }
-    });
-  }
-  
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
-
-  dialogElement.addEventListener("click", closeOnBackDropClick)
-
-function closeOnBackDropClick({ currentTarget, target }) {
-  const dialogElement = currentTarget
-  const isClickedOnBackDrop = target === dialogElement
-  if (isClickedOnBackDrop) {
-    dialogElement.close()
-  }
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
 }
 
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
 
+for (let elm of elements) {
+  observer.observe(elm);
+}
 
+// закрытия диалогового окна если клацать мимо
+dialogElement.addEventListener("click", closeOnBackDropClick);
+
+function closeOnBackDropClick({ currentTarget, target }) {
+  const dialogElement = currentTarget;
+  const isClickedOnBackDrop = target === dialogElement;
+  if (isClickedOnBackDrop) {
+    dialogElement.close();
+  }
+}
