f2f0f65b704dda1d3ba05a1d7d0f509bb05f71f4
Denis Filatov <den.2797@bk.ru>
2024-11-13 23:17:27 +0300

    q

---
+++ styles/script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const video = document.getElementById('background-video');
+
+  
+  // Проверка на воспроизведение видео
+  const playVideo = () => {
+      video.play().catch((error) => {
+          // Обработка ошибки воспроизведения
+          console.error('Ошибка воспроизведения видео:', error);
+          // Попробуйте воспроизвести видео снова после некоторой задержки
+          setTimeout(() => {
+              video.play();
+          }, 1000);
+      });
+  };
+  
+  // Попытка воспроизведения видео
+  playVideo();
+
+  // Добавьте обработчик событий нажатия на экран для повышения вероятности воспроизведения
+  window.addEventListener('touchstart', playVideo, { once: true });
+});
+
+// анимация скрола
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+  
+
+  // анимация слайда
+  
+//   function modalClose (modal) {
+//     modal.classList.add('disable');
+//     modal.close();
+// }
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const modal = document.querySelector('dialog');
+//     const slider = modal.querySelector('.slider');
+//     modal.showModal();
+
+//     slider.addEventListener('change', evt => {
+//         if (evt.target.value !== '100') {
+//             evt.target.value = 0;
+//             return;
+//         }
+
+//         modalClose(modal);
+//     });
+
+// });
